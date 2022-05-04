a2ce14065d95528a60035d9f6ac1408b0f440c6e
Durjoy Barua <83454101+Klausdurjoy@users.noreply.github.com>
2022-05-04 23:44:21 +0600

    home page

--- app/src/main/java/com/example/letsshop/MainActivity.java
+++ app/src/main/java/com/example/letsshop/MainActivity.java
@@ -1,15 +1,54 @@
 package com.example.letsshop;
 
+import static com.example.letsshop.R.menu.search;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(search,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Search Our Product Here");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String query){
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+
+                return true;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
 
